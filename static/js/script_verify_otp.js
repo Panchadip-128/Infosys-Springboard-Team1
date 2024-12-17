41fe67af1bb8867e5107e108caf18fcd39e5123d
SCS Balaji <chaitanyasatyabalaji@gmail.com>
2024-12-18 00:04:24 +0530

    Add verification code page with styling and form submission script

---
+++ static/js/script_verify_otp.js
@@ -0,0 +1,5 @@
+// Placeholder script for form submission
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Next Step: Verification!');
+});

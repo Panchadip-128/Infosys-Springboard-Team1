3cca2157d3bb49eb07841116ac4f379e71727529
SCS Balaji <chaitanyasatyabalaji@gmail.com>
2024-12-18 21:52:29 +0530

    Refactor forgot password and OTP verification forms for improved validation and navigation

--- static/js/script_verify_otp.js
+++ static/js/script_verify_otp.js
@@ -1,5 +1,4 @@
 // Placeholder script for form submission
 document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
     alert('Next Step: Verification!');
 });
